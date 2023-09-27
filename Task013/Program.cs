9893346637be15512edca0ba25b6d7ea86ffe200
Belov <56963330+belof@users.noreply.github.com>
2023-09-27 21:37:31 +0300

    task013

---
+++ Task013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите положительное число");
+char[] digits = Console.ReadLine().ToCharArray();
+
+ if ( digits.Length > 2)
+ {
+
+    Console.Write("Третья цифра в числе: ");
+    Console.Write(digits);
+    Console.Write(", равняется: ");
+    Console.WriteLine(digits[2]);
+ }
+else
+{
+    Console.Write("Третья цифра в числе: ");
+    Console.Write(digits);
+    Console.WriteLine(", отсутствует!");
+}
